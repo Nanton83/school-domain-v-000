970a083a05013a04b9d46c4d494975b9ec3eaac8
Nick Romans <nicholas.romans@gmail.com>
2018-09-10 15:54:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,11 +6,10 @@ class School
 
   def initialize(school_name)
     @school_name = school_name
+    @roster = {}
   end
   
-  def roster
-    @roster = {}
-  end 
+ 
   
   def add_student(name, grade)
 
